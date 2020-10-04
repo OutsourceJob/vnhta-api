bb9445523681147eae226af791586e486adb379b
Chau Nguyen <nguyentrungchau1996@gmail.com>
2020-10-04 16:50:57 +0700

    Update relationship data collecting method and quality of life

--- src/modules/article/quality-of-life/quality-of-life.service.ts
+++ src/modules/article/quality-of-life/quality-of-life.service.ts
@@ -3,17 +3,11 @@ import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Repository } from 'typeorm';
 import { QualityOfLifeEntity } from './quality-of-life.entity';
 
-export class QualityOfLifeService extends TypeOrmCrudService<
-  QualityOfLifeEntity
-> {
+export class QualityOfLifeService extends TypeOrmCrudService<QualityOfLifeEntity> {
   constructor(
     @InjectRepository(QualityOfLifeEntity)
     repo: Repository<QualityOfLifeEntity>,
   ) {
     super(repo);
   }
-
-//   async createQualityOfLife(): Promise<QualityOfLifeEntity> {
-//     const newQualityOfLife = await this.repo.create()
-//   }
 }
