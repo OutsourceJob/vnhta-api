31e803c6ec46253c4a09dc8d08f223b56ce8bcc5
Chau Nguyen <nguyentrungchau1996@gmail.com>
2020-10-04 17:31:20 +0700

    Update relationship

---
+++ src/modules/article/quality-of-life/quality-of-life.dto.ts
@@ -0,0 +1,17 @@
+export class WriteQualityOfLifeDTO {
+   isActive?: boolean;
+   articleId: number;
+   pathologyId?: number;
+   icd20Id?: number;
+   interventionIdArray?: number[];
+   studyLocationIdArray?: number[];
+   studyDesignId?: number;
+   dataCollectingMethodId?: number;
+   qualityOfLifeToolkit?: string;
+   sampleSizeId?: number;
+   samplingMethodId?: number;
+   inclusionCriteria?: string;
+   exclusionCriteria?: string;
+   startSamplingTime?: Date;
+   endSamplingTime?: Date;
+}
--- src/modules/article/quality-of-life/quality-of-life.service.ts
+++ src/modules/article/quality-of-life/quality-of-life.service.ts
@@ -1,13 +1,20 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { InterventionService } from 'src/modules/catalog/intervention/intervention.service';
+import { StudyLocationService } from 'src/modules/catalog/study-location/study-location.service';
 import { Repository } from 'typeorm';
+import { WriteQualityOfLifeDTO } from './quality-of-life.dto';
 import { QualityOfLifeEntity } from './quality-of-life.entity';
 
 export class QualityOfLifeService extends TypeOrmCrudService<QualityOfLifeEntity> {
   constructor(
-    @InjectRepository(QualityOfLifeEntity)
-    repo: Repository<QualityOfLifeEntity>,
+    @InjectRepository(QualityOfLifeEntity) repo: Repository<QualityOfLifeEntity>,
+    private studyLocationService: StudyLocationService,
+    private interventionService: InterventionService
   ) {
     super(repo);
+  }
+
+  async createQualityOfLife(data: WriteQualityOfLifeDTO) {
   }
 }
