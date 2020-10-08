df57aaf6220fcd572fcef1b113281b726779410a
Hackagon <phonghiavan@gmail.com>
2020-10-08 21:06:42 +0700

    modify catalog

--- src/data/catalogs.ts
+++ src/data/catalogs.ts
@@ -118,3 +118,106 @@ export const studyPerspectives = [
     name: "Hệ thống y tế"
   }
 ]
+
+export const parameters = [
+  {
+    id: 1,
+    code: "n",
+    name: "N"
+  },
+  {
+    id: 2,
+    code: "percent",
+    name: "%"
+  },
+  {
+    id: 3,
+    code: "mean",
+    name: "Mean"
+  },
+  {
+    id: 4,
+    code: "median",
+    name: "Median"
+  },
+  {
+    id: 5,
+    code: "sd",
+    name: "SD"
+  },
+  {
+    id: 6,
+    code: "se",
+    name: "SE"
+  },
+  {
+    id: 7,
+    code: "min",
+    name: "Min"
+  },
+  {
+    id: 8,
+    code: "max",
+    name: "Max"
+  },
+  {
+    id: 9,
+    code: "iqr_25",
+    name: "IQR 25%"
+  },
+  {
+    id: 10,
+    code: "iqr_75",
+    name: "IQR 75%"
+  },
+  {
+    id: 11,
+    code: "test",
+    name: "Test"
+  },
+  {
+    id: 12,
+    code: "mean_diff",
+    name: "Mean Difference"
+  },
+  {
+    id: 13,
+    code: "lower_95",
+    name: "95% CI lower"
+  },
+  {
+    id: 14,
+    code: "upper_95",
+    name: "95% CI upper"
+  },
+  {
+    id: 15,
+    code: "p_value",
+    name: "p value"
+  },
+  {
+    id: 16,
+    code: "test_value",
+    name: "Test value"
+  },
+  {
+    id: 17,
+    code: "cost",
+    name: "Cost"
+  },
+  {
+    id: 18,
+    code: "effectiveness",
+    name: "Effectiveness"
+  },
+  {
+    id: 19,
+    code: "icer",
+    name: "ICER"
+  },
+  {
+    id: 20,
+    code: "icer_result",
+    name: "Result"
+  }
+]
