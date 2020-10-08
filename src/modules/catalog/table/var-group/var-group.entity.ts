e4bf828938af28d86e624c4e52192682a6919f7e
Hackagon <phonghiavan@gmail.com>
2020-10-08 09:51:43 +0700

    fix name

--- src/modules/catalog/table/var-group/var-group.entity.ts
+++ src/modules/catalog/table/var-group/var-group.entity.ts
@@ -6,7 +6,7 @@ export class VarGroupEntity extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ name: "var" })
+  @Column()
   name: string;
 
   @CreateDateColumn({ name: "created_at" })
