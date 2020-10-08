7b5423e3a67775ecd0d8521e937c264846a03b4a
Hackagon <phonghiavan@gmail.com>
2020-10-08 17:36:53 +0700

    catalog

---
+++ src/data/catalogs.ts
@@ -0,0 +1,120 @@
+export const studyDesigns = [
+  {
+    id: 1,
+    name: "Mô tả",
+  },
+  {
+    id: 2,
+    name: "Can thiệp",
+  },
+  {
+    id: 3,
+    name: "Mô hình",
+  }
+]
+
+export const dataCollectingMethods = [
+  {
+    id: 1,
+    name: "Hồi cứu",
+  },
+  {
+    id: 2,
+    name: "Tiến cứu",
+  },
+  {
+    id: 3,
+    name: "Cắt ngang",
+  }
+]
+
+export const sampleSizes = [
+  {
+    id: 1,
+    name: "Toàn bộ",
+  },
+  {
+    id: 2,
+    name: "Theo công thức"
+  }
+]
+
+export const samplingMethods = [
+  {
+    id: 1,
+    name: "Không chọn mẫu",
+  },
+  {
+    id: 2,
+    name: "Ngẫu nhiên đơn"
+  },
+  {
+    id: 3,
+    name: "Ngẫu nhiên hệ thống"
+  },
+  {
+    id: 4,
+    name: "Ngẫu nhiên phân tầng"
+  },
+  {
+    id: 5,
+    name: "Ngẫu nhiên thuận tiện"
+  }
+]
+
+export const costTypes = [
+  {
+    id: 1,
+    name: "Theo năm"
+  },
+  {
+    id: 2,
+    name: "Theo khóa"
+  },
+  {
+    id: 3,
+    name: "Theo đợt"
+  },
+  {
+    id: 4,
+    name: "Theo ca"
+  }
+]
+
+export const costComponents = [
+  {
+    id: 1,
+    name: "TTYT"
+  },
+  {
+    id: 2,
+    name: "TTNYT"
+  },
+  {
+    id: 3,
+    name: "GT"
+  }
+]
+
+export const studyPerspectives = [
+  {
+    id: 1,
+    name: "Xã hội"
+  },
+  {
+    id: 2,
+    name: "Người chi trả"
+  },
+  {
+    id: 3,
+    name: "Người bệnh"
+  },
+  {
+    id: 4,
+    name: "Cơ sở y tế"
+  },
+  {
+    id: 5,
+    name: "Hệ thống y tế"
+  }
+]
