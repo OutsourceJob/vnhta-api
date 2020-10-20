b83b5af35f354b6e03a09eff71acd79c962b8d4d
Chau Nguyen <nguyentrungchau1996@gmail.com>
2020-10-20 17:37:19 +0700

    update

--- src/modules/article/quality-of-life/quality-of-life.entity.ts
+++ src/modules/article/quality-of-life/quality-of-life.entity.ts
@@ -46,16 +46,15 @@ export class QualityOfLifeEntity extends BaseEntity {
   @Column({ name: 'pathology_id', nullable: true })
   pathologyId: number;
 
-  // @ManyToOne(
-  //   type => Icd20Entity,
-  //   i => i.qualityOfLives,
-  //   {
-  //     onDelete: 'SET NULL',
-  //   },
-  // )
-  // @JoinColumn({ name: 'icd20_id' })
-  // @Column({ name: 'icd20_id', nullable: true })
-  // icd20Id: number;
+  @ManyToMany(
+    type => Icd20Entity,
+  )
+  @JoinTable({
+    name: 'quality_of_life_icd_20',
+    joinColumns: [{ name: 'quality_of_life_id' }],
+    inverseJoinColumns: [{ name: 'icd_20_id' }]
+  })
+  icd20s: Icd20Entity[];
 
   @ManyToMany(
     type => InterventionEntity,
@@ -79,42 +78,42 @@ export class QualityOfLifeEntity extends BaseEntity {
   })
   studyLocations: StudyLocationEntity[];
 
-  @ManyToOne(type => StudyDesignEntity, s => s.qualityOfLives, {
-    onDelete: "SET NULL"
-  })
-  @JoinColumn({ name: 'study_design_id' })
+  // @ManyToOne(type => StudyDesignEntity, s => s.qualityOfLives, {
+  //   onDelete: "SET NULL"
+  // })
+  // @JoinColumn({ name: 'study_design_id' })
   @Column({ name: 'study_design_id', nullable: true })
   studyDesignId: number;
 
-  @ManyToOne(type => DataCollectingMethodEntity, d => d.qualityOfLives, {
-    onDelete: "SET NULL"
-  })
-  @JoinColumn({ name: 'data_collecting_method_id' })
+  // @ManyToOne(type => DataCollectingMethodEntity, d => d.qualityOfLives, {
+  //   onDelete: "SET NULL"
+  // })
+  // @JoinColumn({ name: 'data_collecting_method_id' })
   @Column({ name: 'data_collecting_method_id', nullable: true })
   dataCollectingMethodId: number;
 
   @Column({ name: 'quality_of_life_toolkit', nullable: true })
   qualityOfLifeToolkit: string;
 
-  @ManyToOne(type => SampleSizeEntity, s => s.qualityOfLives, {
-    onDelete: "SET NULL"
-  })
-  @JoinColumn({ name: 'sample_size_id' })
+  // @ManyToOne(type => SampleSizeEntity, s => s.qualityOfLives, {
+  //   onDelete: "SET NULL"
+  // })
+  // @JoinColumn({ name: 'sample_size_id' })
   @Column({ name: 'sample_size_id', nullable: true })
   sampleSizeId: number;
-
-  @ManyToOne(type => SamplingMethodEntity, s => s.qualityOfLives, {
-    onDelete: "SET NULL"
-  })
-  @JoinColumn({ name: 'sampling_method_id' })
-  @Column({ name: 'sampling_method_id', nullable: true })
-  samplingMethodId: number;
 
   @Column({ name: 'inclusion_criteria', nullable: true })
   inclusionCriteria: string;
 
   @Column({ name: 'exclusion_criteria', nullable: true })
   exclusionCriteria: string;
+
+  // @ManyToOne(type => SamplingMethodEntity, s => s.qualityOfLives, {
+  //   onDelete: "SET NULL"
+  // })
+  // @JoinColumn({ name: 'sampling_method_id' })
+  @Column({ name: 'sampling_method_id', nullable: true })
+  samplingMethodId: number;
 
   @Column({ name: 'start_sampling_time', nullable: true })
   startSamplingTime: Date;
