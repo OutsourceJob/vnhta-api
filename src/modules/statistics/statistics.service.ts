0c79351b86e11292301b3a85c8f955b88a203b9c
Chau Nguyen <nguyentrungchau1996@gmail.com>
2020-10-22 16:25:19 +0700

    Update statistics

--- src/modules/statistics/statistics.service.ts
+++ src/modules/statistics/statistics.service.ts
@@ -152,7 +152,7 @@ export class StatisticsService {
             LEFT JOIN 
               cost_benefit ON cost_benefit.article_id = article.id
             WHERE 
-              article.id IN (1, 2, 3, 4, 5, 6) AND
+              article.id IN (${_.toString(articleIdArray)}) AND
               cost_benefit.data_collecting_method_id_array LIKE "%${method.id}%"
           `)
       }
@@ -167,7 +167,7 @@ export class StatisticsService {
       LEFT JOIN 
         cost_benefit ON cost_benefit.article_id = article.id
       WHERE 
-        article.id IN (1, 2, 3, 4, 5, 6) AND
+        article.id IN (${_.toString(articleIdArray)}) AND
         cost_benefit.data_collecting_method_id_array REGEXP "1"
       ${res}
     `)
@@ -252,6 +252,216 @@ export class StatisticsService {
         article.id IN (${_.toString(articleIdArray)}) AND
         cost_benefit.cost_component_id_array REGEXP "1"
       ${res}
+    `)
+  }
+
+  async getYearOfCostStatistics(articleIdArray: number[]): Promise<any[]> {
+    return await this.connection.query(`
+      SELECT 
+        cost_benefit.year_of_cost, 
+        COUNT(*) AS quantity
+      FROM
+        article
+      LEFT JOIN 
+        cost_benefit ON cost_benefit.article_id = article.id
+      WHERE 
+        article.id IN (${_.toString(articleIdArray)})
+      GROUP BY 
+      cost_benefit.year_of_cost;
+    `)
+  }
+
+  async getStudyPerspectiveStatistics(articleIdArray: number[]): Promise<any[]> {
+    const res = _.chain(catalogs.studyPerspectives).map(item => {
+      if (item.id >= 2) {
+        return (`
+            UNION
+            SELECT 
+              "${item.id}" AS label,
+              COUNT(*) AS quantity
+            FROM
+              article
+            LEFT JOIN 
+              cost_benefit ON cost_benefit.article_id = article.id
+            WHERE 
+              article.id IN (${_.toString(articleIdArray)}) AND
+              cost_benefit.study_perspective_id_array LIKE "%${item.id}%"
+          `)
+      }
+    }).join('').value();
+
+    return await this.connection.query(`
+      SELECT 
+        "1" AS label,
+        COUNT(*) AS quantity
+      FROM
+        article
+      LEFT JOIN 
+        cost_benefit ON cost_benefit.article_id = article.id
+      WHERE 
+        article.id IN (${_.toString(articleIdArray)}) AND
+        cost_benefit.study_perspective_id_array REGEXP "1"
+      ${res}
+    `)
+  }
+
+  async getQLPathologyStatistics(articleIdArray: number[]) {
+    return await this.connection.query(`
+      SELECT 
+        pathology.name AS pathology,
+        COUNT(*) AS quantity
+      FROM
+        article
+      LEFT JOIN 
+        quality_of_life ON quality_of_life.article_id = article.id
+      LEFT JOIN 
+        pathology ON pathology.id = quality_of_life.pathology_id
+      WHERE 
+        article.id IN (${_.toString(articleIdArray)})
+      GROUP BY 
+        pathology.id
+    `)
+  }
+
+  async getQLIcd20Statistics(articleIdArray: number[]): Promise<any[]> {
+    return await this.connection.query(`
+      SELECT 
+        icd_20.code AS icd20,
+        COUNT(*) AS quantity
+      FROM
+        article
+      LEFT JOIN 
+        quality_of_life ON quality_of_life.article_id = article.id
+      LEFT JOIN 
+        quality_of_life_icd_20 ON quality_of_life_icd_20.quality_of_life_id = quality_of_life.id
+      LEFT JOIN 
+        icd_20 ON quality_of_life_icd_20.icd_20_id = icd_20.id
+      WHERE 
+        article.id IN (${_.toString(articleIdArray)})
+      GROUP BY 
+        icd_20.id
+    `)
+  }
+
+  async getQLInterventionStatistics(articleIdArray: number[]): Promise<any[]> {
+    return await this.connection.query(`
+      SELECT 
+        intervention.name AS intervention,
+        COUNT(*) AS quantity
+      FROM
+        article
+      LEFT JOIN 
+        quality_of_life ON quality_of_life.article_id = article.id
+      LEFT JOIN 
+        quality_of_life_intervention ON quality_of_life_intervention.quality_of_life_id = quality_of_life.id
+      LEFT JOIN 
+        intervention ON quality_of_life_intervention.intervention_id = intervention.id
+      WHERE 
+        article.id IN (${_.toString(articleIdArray)})
+      GROUP BY 
+        intervention.id
+    `)
+  }
+
+  async getQLStudyLocationStatistics(articleIdArray: number[]): Promise<any[]> {
+    return await this.connection.query(`
+      SELECT 
+        study_location.name AS studyLocation,
+        COUNT(*) AS quantity
+      FROM
+        article
+      LEFT JOIN 
+        quality_of_life ON quality_of_life.article_id = article.id
+      LEFT JOIN 
+        quality_of_life_study_location ON quality_of_life_study_location.quality_of_life_id = quality_of_life.id
+      LEFT JOIN 
+        study_location ON quality_of_life_study_location.study_location_id = study_location.id
+      WHERE 
+        article.id IN (${_.toString(articleIdArray)})
+      GROUP BY 
+        study_location.id
+    `)
+  }
+
+  async getQLStudyDesignStatistics(articleIdArray: number[]): Promise<any[]> {
+    return await this.connection.query(`
+      SELECT 
+        quality_of_life.study_design_id AS studyDesignId,
+        COUNT(*) AS quantity
+      FROM
+        article
+      LEFT JOIN 
+        quality_of_life ON quality_of_life.article_id = article.id
+      WHERE 
+        article.id IN (${_.toString(articleIdArray)})
+      GROUP BY 
+        quality_of_life.study_design_id
+    `)
+  }
+
+  async getQLDataCollectingMethodStatistics(articleIdArray: number[]): Promise<any[]> {
+    const res = _.chain(catalogs.dataCollectingMethods).map(method => {
+      if (method.id >= 2) {
+        return (`
+            UNION
+            SELECT 
+              "${method.id}" AS label,
+              COUNT(*) AS quantity
+            FROM
+              article
+            LEFT JOIN 
+              quality_of_life ON quality_of_life.article_id = article.id
+            WHERE 
+              article.id IN (${_.toString(articleIdArray)}) AND
+              quality_of_life.data_collecting_method_id_array LIKE "%${method.id}%"
+          `)
+      }
+    }).join('').value();
+
+    return await this.connection.query(`
+      SELECT 
+        "1" AS label,
+        COUNT(*) AS quantity
+      FROM
+        article
+      LEFT JOIN 
+        quality_of_life ON quality_of_life.article_id = article.id
+      WHERE 
+        article.id IN (${_.toString(articleIdArray)}) AND
+        quality_of_life.data_collecting_method_id_array REGEXP "1"
+      ${res}
+    `)
+  }
+
+  async getQLSampleSizeStatistics(articleIdArray: number[]): Promise<any[]> {
+    return await this.connection.query(`
+      SELECT 
+        quality_of_life.sample_size_id AS samplesSize,
+        COUNT(*) AS quantity
+      FROM
+        article
+      LEFT JOIN 
+        quality_of_life ON quality_of_life.article_id = article.id
+      WHERE 
+        article.id IN (${_.toString(articleIdArray)})
+      GROUP BY 
+        quality_of_life.sample_size_id
+    `)
+  }
+
+  async getQLSamplingMethodStatistics(articleIdArray: number[]): Promise<any[]> {
+    return await this.connection.query(`
+      SELECT 
+        quality_of_life.sampling_method_id AS samplingMethod,
+        COUNT(*) AS quantity
+      FROM
+        article
+      LEFT JOIN 
+        quality_of_life ON quality_of_life.article_id = article.id
+      WHERE 
+        article.id IN (${_.toString(articleIdArray)})
+      GROUP BY 
+        quality_of_life.sampling_method_id
     `)
   }
 }
