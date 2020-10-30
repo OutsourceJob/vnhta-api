78d61d776f07ea53c16bd06c934574a989a628ee
Hackagon <phonghiavan@gmail.com>
2020-10-30 18:25:42 +0700

    clear code

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,24 +13,12 @@ import { JournalModule } from './modules/catalog/journal/journal.module';
 import { Icd20Module } from './modules/catalog/icd-20/icd-20.module';
 import { InterventionModule } from './modules/catalog/intervention/intervention.module';
 import { StudyLocationModule } from './modules/catalog/study-location/study-location.module';
-import { SamplingMethodModule } from './modules/catalog/sampling-method/sampling-method.module';
-import { StudyDesignModule } from './modules/catalog/study-design/study-design.module';
 import { DataCollectingMethodModule } from './modules/catalog/data-collecting-method/data-collecting-method.module';
-import { CostTypeModule } from './modules/catalog/cost-type/cost-type.module';
-import { CostComponentModule } from './modules/catalog/cost-component/cost-component.module';
-import { StudyPerspectiveModule } from './modules/catalog/study-perspective/study-perspective.module';
-import { SampleSizeModule } from './modules/catalog/sample-size/sample-size.module';
 import { ComparatorModule } from "./modules/catalog/comparator/comparator.module";
-import { ModelTypeModule } from "./modules/catalog/model-type/model-type.module";
-import { EffectivenessDataCollectingMethodModule } from './modules/catalog/effectiveness-data-collecting-method/effectiveness-data-collecting-method.module';
 import { DiscountRateModule } from './modules/catalog/discount-rate/discount-rate.module';
-import { CurrencyUnitModule } from './modules/catalog/currency-unit/currency-unit.module';
 import { AnalysisMethodModule } from './modules/catalog/analysis-method/analysis-method.module';
-import { HeterogeneityAnalysisModule } from './modules/catalog/heterogeneity-analysis/heterogeneity-analysis.module';
 import { UncertaintyAnalysisMethodModule } from './modules/catalog/uncertainty-analysis-method/uncertainty-analysis-method.module';
-import { UncertaintyAnalysisResultModule } from "./modules/catalog/uncertainty-analysis-result/uncertainty-analysis-result.module";
 import { TableModule } from './modules/catalog/table/table.module';
-import { CEStudyDesignModule } from './modules/catalog/ce-study-design/ce-study-design.module';
 import { StatisticsModule } from './modules/statistics/statistics.module';
 
 @Module({
@@ -56,22 +44,11 @@ import { StatisticsModule } from './modules/statistics/statistics.module';
     Icd20Module,
     InterventionModule,
     StudyLocationModule,
-    SamplingMethodModule,
-    StudyDesignModule,
     DataCollectingMethodModule,
-    CostTypeModule, CostComponentModule,
-    StudyPerspectiveModule,
-    SampleSizeModule,
-    ModelTypeModule,
     ComparatorModule,
-    EffectivenessDataCollectingMethodModule,
     DiscountRateModule,
-    CurrencyUnitModule,
     AnalysisMethodModule,
-    HeterogeneityAnalysisModule,
     UncertaintyAnalysisMethodModule,
-    UncertaintyAnalysisResultModule,
-    CEStudyDesignModule,
 
     // table
     TableModule,
--- src/modules/catalog/table/row/row.module.ts
+++ src/modules/catalog/table/row/row.module.ts
@@ -4,14 +4,12 @@ import { RowEntity } from './row.entity';
 import { RowController } from './row.controller';
 import { RowService } from './row.service';
 import { FeatureModule } from '../feature/feature.module';
-import { ParameterModule } from '../parameter/parameter.module';
 import { TableModule } from '../table.module';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([RowEntity]),
         FeatureModule,
-        // ParameterModule,
         forwardRef(() => TableModule)
     ],
     controllers: [RowController],
